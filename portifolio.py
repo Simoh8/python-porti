cc9086dbd6aaf1a657cf29c7d026771b70b3ae8b
simon muturi <simomutu@gmail.com>
2023-05-26 16:04:22 +0300

    python version

---
+++ portifolio.py
@@ -0,0 +1,6 @@
+import streamlit as sm
+sm.set_page_config(page_title= "Simon Muturi", page_icon="https://img.icons8.com/?size=1x&id=KkP-jZEDenKD&format=png", layout="wide")
+sm.subheader("Hi i am Dev Simon Muturi")
+sm.title("A Software Developer From Kenya ")
+sm.write("Im working on a portfolio in python for today i hope to progress as i learn more ") 
+sm.write("[My Github account is > ]( https://github.com/Simoh8)")
